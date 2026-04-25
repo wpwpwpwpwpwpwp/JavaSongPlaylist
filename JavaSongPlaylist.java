e661ad4fae9b16f0b57a266dce529e37bcc936c1
Wonjoon Park <wpark814@terpmail.umd.edu>
2026-04-25 17:00:03 -0400

    JSPUpdate

--- JavaSongPlaylist.java
+++ JavaSongPlaylist.java
@@ -3,4 +3,9 @@ import java.util.ArrayList;
 public class JavaSongPlaylist{
     private String playlistName;
     private ArrayList<JavaSong> songs; //helohelo 
+
+    public JavaSongPlaylist(String playlistName){ //initialize playlist
+        this.playlistName = playlistName;
+        songs = new ArrayList<>(); 
+    }
 }
