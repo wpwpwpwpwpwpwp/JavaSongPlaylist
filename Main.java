50921d35bcea5494716ce0bfb62e1797d4c27a2e
Wonjoon Park <wpark814@terpmail.umd.edu>
2026-04-26 20:25:09 -0400

    mainupdate

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[]args){
         JavaSong track1 = new JavaSong("look at the sea", "oiscle melonpan", false);
+        JavaSong track2 = new JavaSong("Love is Stronger Than Pride", "Sade", false);
         System.out.println(track1);
+        System.out.println(track1.isPlatinum());
     }
 }
